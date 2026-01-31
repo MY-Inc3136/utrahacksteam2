b8ac2489a1a3f90e40571040e7ecbe61fbd70f36
Meil C <meilc@example.com>
2026-01-31 18:27:29 -0500

    added ultrasonic sensor detection

--- avoid_obstruction/avoid_obstruction.ino
+++ avoid_obstruction/avoid_obstruction.ino
@@ -2,6 +2,12 @@
  * Navigate around obstruction using set routine and go back to path as usual
  */
 
+const int ULTRASONIC_TRIG_PIN = 10;
+const int ULTRASONIC_OUT_PIN = 11;
+const int ULTRASONIC_ECHO_PIN = 12;
+
+int obstruction_range = 5;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -9,6 +15,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // reroute if obstruction detected
+  if (obstruction_detected()) 
+    reroute();
+  
 
 }
 
@@ -20,5 +31,41 @@ void reroute() {
   rotateLeft(90);
   moveForward(100); // adjust again
   rotateRight(90); // adjust again
+  
+}
+
+long get_distance() {
+
+  long duration;
+  
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+
+  duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH, 20000);
+
+  return duration * 0.034 / 2;
+  
+}
+
+bool obstruction_detected() {
+  long distance = get_distance();
+
+  // FOR DEBUGGING
+  Serial.print("Obstruction detected at");
+  Serial.print(distance);
+  Serial.print(" cm, "); 
+  
+  // If the distance to the nearest object is within range
+  if(distance <= obstruction_range) {
+    Serial.println("within range => reroute");
+    return true; // TRUE, there is an obstruction detected
+    
+  } else {
+    Serial.println("not within range => stay on path");
+    return false; // FALSE, there is no obstruction detected
+  }
   
 }
