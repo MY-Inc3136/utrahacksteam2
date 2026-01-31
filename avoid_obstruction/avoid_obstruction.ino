e26a7bd898341c03d895b4c511d84b10a03f8099
Meil C <meilc@example.com>
2026-01-31 14:46:44 -0500

    rough logic for avoiding obstruction

--- avoid_obstruction/avoid_obstruction.ino
+++ avoid_obstruction/avoid_obstruction.ino
@@ -1,3 +1,7 @@
+/* Detect obstructions
+ * Navigate around obstruction using set routine and go back to path as usual
+ */
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +10,15 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void reroute() {
+  // Move right to get off the path
+  moveRight(50); // adjust timing based on how long it needs to move right for
+  rotateLeft(90); // assuming it's in degrees
+  moveForward(100); // adjust based on how long the obstruction is 
+  rotateLeft(90);
+  moveForward(100); // adjust again
+  rotateRight(90); // adjust again
+  
 }
